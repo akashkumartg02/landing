70ce0e2d738f58196999b9027b09f62555e16a5a
Akash Kumar <122004642+akash-kumar-cmdk@users.noreply.github.com>
2023-08-10 16:34:50 +0530

    sdk updated to stage

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CmdKConfigurer } from '@cmdk/react-ui-toolkit';
+import { CmdKConfigurer } from '@cmdk/stage-toolkit';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
