b61581a08098fe5f3f152ca8b21d447fc7d6aa02
Akash Kumar <122004642+akash-kumar-cmdk@users.noreply.github.com>
2023-08-23 19:23:44 +0530

    token changed

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { CmdKConfigurer } from '@cmdk/stage-toolkit';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CmdKConfigurer token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdJZCI6IjIiLCJ1c2VySWQiOiIyIiwiZG9tYWluIjoiaHR0cHM6Ly9sYW5kaW5nLWFrLm5ldGxpZnkuYXBwLyIsImlhdCI6MTY5MjcwNzY4MCwiZXhwIjoxNjkzMzEyNDgwfQ.ZXrOURq_WZZqULu57jP8pI_vnNkletogrh2ODPJgs4Y'>
+    <CmdKConfigurer token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcmdJZCI6IjIiLCJ1c2VySWQiOiIyIiwiZG9tYWluIjoiXHUwMDAw77-9c1x1MDAwNj3vv71xM3jvv70p77-977-9by0qXFzvv73vv70ua--_vUgpO--_ve-_ve-_vVx1MDAwMdaO77-977-977-9Xyzvv73vv73vv71k77-9OlFuIiwiaWF0IjoxNjkyNzk4Njk0LCJleHAiOjE2OTM0MDM0OTR9.dt3FXJ_f3meURdmyeX_HHszlx8gU3kOz8xb_omYG_hg'>
       <App />
     </CmdKConfigurer>
   </React.StrictMode>
